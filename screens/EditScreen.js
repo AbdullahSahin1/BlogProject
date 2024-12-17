8a38674e1806b62512a6047cc128520430bbada2
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-17 14:26:33 +0300

    Update EditScreen.js

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { StyleSheet,Text, View } from 'react-native';
+
+const EditScreen = () => {
+    return (
+        <View>
+            <Text>Edit Screen</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default EditScreen;
 
