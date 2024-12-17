06dca2f6f1f7e41c864ebadaf5f4002ecff9e967
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-17 12:49:59 +0300

    Update IndexScreen.js

--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View, FlatList,Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import React, { useContext } from 'react';
 import BlogContext from '../context/BlogContext';
 
 export default function IndexScreen() {
-  const {data,addBlogPost} = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       {/* <Text>IndexScreen </Text> */}
-      <Button title='Ekle' onPress={addBlogPost}></Button>
+      <Button title="Ekle" onPress={addBlogPost} />
       <FlatList
         data={data}
         keyExtractor={(blogPost) => blogPost.title}
