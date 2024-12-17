612643e4aeb201ce34c6b2ca491e1111d5826bba
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-17 15:37:51 +0300

    Update IndexScreen.js

--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -5,27 +5,31 @@ import {
   FlatList,
   Button,
   TouchableOpacity,
-} from "react-native";
-import React, { useContext } from "react";
-import { Context } from "../context/BlogContext";
-import Feather from "@expo/vector-icons/Feather";
-export default function IndexScreen({navigation}) {
-  const { state, addBlogPost,deleteBlockPost } = useContext(Context);
+} from 'react-native';
+import React, { useContext } from 'react';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
+
+export default function IndexScreen({ navigation }) {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
+      {/* <Text>IndexScreen </Text> */}
       <Button title="Ekle" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.id}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity onPress={() => navigation.navigate('Show',{id:item.id})}>
-            <View style={styles.row}>
-              <Text style={styles.title}>{item.title}</Text>
-              <TouchableOpacity onPress={() => deleteBlockPost(item.id)}>
-                <Feather name="trash" size={24} color="black" />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Show', { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather name="trash" size={24} color="black" />
+                </TouchableOpacity>
+              </View>
             </TouchableOpacity>
           );
         }}
@@ -36,12 +40,12 @@ export default function IndexScreen({navigation}) {
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderTopWidth: 1,
     paddingHorizontal: 10,
     paddingVertical: 20,
-    borderColor: "gray",
+    borderColor: 'gray',
   },
   title: {
     fontSize: 18,
