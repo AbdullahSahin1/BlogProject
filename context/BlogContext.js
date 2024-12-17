ece088d02f471f85d05b158c037e6cfbc489cae5
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-17 13:15:56 +0300

    Update BlogContext.js

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,31 +1,23 @@
-import React, { useState, useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import React, { useState, useReducer } from "react";
+import CreateDataContext from "./CreateDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogpost':
-      return [...state, { title: 'Vue Js' }];
+    case "add_blogpost":
+      return [...state, { title: "Vue Js" }];
     default:
       return state;
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, [
-    { title: 'React Native' },
-    { title: 'Javascript' },
-  ]);
-
-  const addBlogPost = () => {
-    dispatch({ type: 'add_blogpost' });
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
   };
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+export const { Context, Provider } = CreateDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
