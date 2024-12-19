cc4ad077993f5d009c9cb75ce4d2058849cf8e95
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-19 17:38:42 +0300

    Update EditScreen.js

--- screens/EditScreen.js
+++ screens/EditScreen.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { StyleSheet,Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useContext } from "react";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
-const EditScreen = () => {
-    return (
-        <View>
-            <Text>Edit Screen</Text>
-        </View>
-    );
+export default function EditScreen({ route, navigation }) {
+  const { state, editBlogPost } = useContext(Context);
+  const id = route.params.id;
+  const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
+  return (
+    <BlogPostForm
+      isEditable={true}
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.navigate("Index"));
+      }}
+    />
+  );
 }
 
-const styles = StyleSheet.create({})
-
-export default EditScreen;
-
+const styles = StyleSheet.create({});
